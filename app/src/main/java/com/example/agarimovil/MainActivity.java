aa6a3964586afbbd7ee6ca82bb057e32cf065290
JuanCarlosRF79 <juan.ruvalcaba@ia-society.com>
2023-06-17 13:39:33 -0600

    Diseño login y registro

--- app/src/main/java/com/example/agarimovil/MainActivity.java
+++ app/src/main/java/com/example/agarimovil/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.agarimovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView registro;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Quitar la barra de arriba
+        //getSupportActionBar().hide();
+
+        registro = findViewById(R.id.txtRegistro);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
     }
 }
